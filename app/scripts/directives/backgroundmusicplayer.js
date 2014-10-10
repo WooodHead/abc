4b5461c4cf4f4ec028fb3c15c8dc7d8f4f2686ae
Kevin Tame <kevintame@kevins-mbp.home>
2014-10-10 08:43:41 -0400

    updated music player

--- app/scripts/directives/backgroundmusicplayer.js
+++ app/scripts/directives/backgroundmusicplayer.js
@@ -9,23 +9,26 @@
 angular.module('abcApp')
 .directive('backgroundMusicPlayer', function () {
 	return {
-		template: '<button class="btn btn-default" id="myBtn">Play</button> <button class="btn btn-default" id="myBtnPause">Mute</button>',
+		template: '<button class="btn btn-default" id="myBtn">Play</button> <button class="btn btn-default" id="mute">Mute</button>',
 		restrict: 'EA',
 		link: function backgroundMusic() {
 			var sound = new Howl({
-				urls: ['resources/song.m4a'],
+				urls: ['resources/song.mp3'],
 				volume: 0.2,
 				loop: true,
 				autoplay: true
 			});
-				function playSound() {
-					sound.play();
+				var mute = document.getElementById('mute');
+				mute.onclick = function() {
+					if (sound === sound.unmute){
+						sound.unmute();
 					}
-				function muteSound() {
-					sound.mute();
+					else {
+						sound.mute();
 					}
-			document.getElementById('myBtn').addEventListener('click', playSound);
-			document.getElementById('myBtnPause').addEventListener('click', muteSound);
-		}
+					};
+				}
 	};
 });
+
+
